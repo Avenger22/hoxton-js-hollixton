dc186b700273b5448d97e0f037e7871b70cb76fb
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2021-12-13 13:51:51 +0100

    Rendering items in the store based on the state

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,45 @@
 const sectionMenusEl = document.querySelector('section.container-menus')
 
 
-//------------------------------STATE OBJECT-------------------------------------
+//-----------------------------------------------STATE OBJECT---------------------------------------------------------------------------------
 
 const state = {
 
-    items: []
+    store: [],
+    users: []
 
 }
 
-//----------------------------END OF STATE OBJECT----------------------------
+//--------------------------------------------END OF STATE OBJECT----------------------------------------------------------------
 
 
-//----------------------------SERVER FUNCTIONS-------------------------------------
+//--------------------------------------------SERVER FUNCTIONS-------------------------------------------------------------------------------
+
+function getStoreArrayFromServer() {
+
+    return fetch('http://localhost:3000/store')        
+        .then(function (response) 
+        {
+            return response.json()
+        })
+
+}
+
+function getUsersArrayFromServer() {
+
+    return fetch('http://localhost:3000/users')        
+        .then(function (response) 
+        {
+            return response.json()
+        })
+
+}
+
+//------------------------------------------END OF SERVER FUNCTIONS---------------------------------------------------------------------
 
 
-//----------------------------END OF SERVER FUNCTIONS-------------------------------------
 
-
-
-//----------------------------RENDER FUNCTIONS-------------------------------------
+//---------------------------------------------RENDER FUNCTIONS------------------------------------------------------------------------------
 
 function renderHeader() {
 
@@ -92,7 +112,7 @@ function renderHeader() {
 
 }
 
-function renderMain() {
+function renderMain(storeArrayParam) {
     
     const mainMenuEl = document.createElement('main')
     mainMenuEl.setAttribute('class', 'main-menu')
@@ -104,30 +124,54 @@ function renderMain() {
     h2El1.setAttribute('class', 'h2-main')
     h2El1.textContent = 'Home'
 
+    divEl1.append(h2El1)
+
+    //here we create he item in the store 
     const divEl2 = document.createElement('div')
     divEl2.setAttribute('class', 'store-items-wrapper')
 
-    const divEl3 = document.createElement('div')
-    divEl3.setAttribute('class', 'store-item')
+    for (const item of storeArrayParam) { //this is now the object in an array iteration
 
-    const imgEl = document.createElement('img')
-    imgEl.setAttribute('src', 'https://img.hollisterco.com/is/image/anf/KIC_359-1220-1911-805_prod1?policy=product-small')
-    imgEl.setAttribute('alt', '')
+        const divEl3 = document.createElement('div')
+        divEl3.setAttribute('class', 'store-item')
 
-    const h2El2 = document.createElement('h2')
-    h2El2.textContent = 'Shirt'
+        const imgEl = document.createElement('img')
+        imgEl.setAttribute('src', `${item.image}`)
+        imgEl.setAttribute('alt', '')
 
-    const spanEl1 = document.createElement('span')
-    spanEl1.setAttribute('id', 'span-1')
-    spanEl1.textContent = '50$'
+        const h2El2 = document.createElement('h2')
+        h2El2.textContent = item.name
 
-    const spanEl2 = document.createElement('span')
-    spanEl2.setAttribute('id', 'span-2')
-    spanEl2.textContent = '20$'
+        //now we check if an propery in in the object to see discounted price or not
+        if (item.hasOwnProperty('discountedPrice')) {
 
-    divEl3.append(imgEl, h2El2, spanEl1, spanEl2)
+            const spanEl1 = document.createElement('span')
+            spanEl1.setAttribute('class', 'span-1 special-span')
+            spanEl1.textContent = `£ ${item.price}`
 
-    divEl2.append(divEl3)
+            const spanEl2 = document.createElement('span')
+            spanEl2.setAttribute('class', 'span-2')
+            spanEl2.textContent = `£ ${item.discountedPrice}`
+
+            divEl3.append(imgEl, h2El2, spanEl1, spanEl2)
+            divEl2.append(divEl3)
+
+        }
+
+        else {
+
+            const spanEl1 = document.createElement('span')
+            spanEl1.setAttribute('class', 'span-1 special-span')
+            spanEl1.textContent = `£ ${item.price}`
+            spanEl1.style.color = '#000'
+            spanEl1.style.textDecoration = 'none'
+
+            divEl3.append(imgEl, h2El2, spanEl1)
+            divEl2.append(divEl3)
+
+        }
+
+    }
 
     mainMenuEl.append(divEl1, divEl2)
 
@@ -166,14 +210,31 @@ function renderFooter() {
 
 function render() {
 
-    // sectionMenusEl.innerHTML = ''
+    sectionMenusEl.innerHTML = ''
 
     renderHeader()
-    renderMain()
+    renderMain(state.store)
     renderFooter()
 
 }
 
-//----------------------------END OF RENDER FUNCTIONS-------------------------------------
+function init() {
 
-render()
+     //FETCHING AND STORING DATA FROM SERVER TO STATE both arrays from json server
+    getStoreArrayFromServer().then(function (storeArrayFromServer) {
+        state.store = storeArrayFromServer
+        render()
+    })
+
+    getUsersArrayFromServer().then(function (usersArrayFromServer) {
+        state.users = usersArrayFromServer
+        render()
+    })
+
+    render()
+
+}
+
+//---------------------------------------END OF RENDER FUNCTIONS---------------------------------------------------------------
+
+init()
